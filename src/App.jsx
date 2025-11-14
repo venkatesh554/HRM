548f7a9fa48714c9287b7b22dc63e13b42b296a2
Varshini Sandepudi <varshinisandepudi@gmail.com>
2025-11-14 13:18:55 +0530

    Recovered Varshini part

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ export default function App() {
   return(
     <>
     <Navbar/>
-    {/*<EmployeeMainDashboard />;*/}
-    <DashboardMain />;
+    {/* <DashboardMain />;*/}
+    <EmployeeMainDashboard />; 
     </>
   )
 }
